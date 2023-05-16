c94eeaa023b0bf40b57b0f5e8caecbb5fe741581
Alumno <73141051@continental.edu.pe>
2023-05-16 16:03:36 -0500

    commit prueba

---
+++ app/src/main/java/com/example/mplusuarios/InsertarUsuario.java
@@ -0,0 +1,18 @@
+package com.example.mplusuarios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InsertarUsuario extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insertar_usuario);
+
+
+    }
+}
--- app/src/main/java/com/example/mplusuarios/MainActivity.java
+++ app/src/main/java/com/example/mplusuarios/MainActivity.java
@@ -3,15 +3,25 @@ package com.example.mplusuarios;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener {
 
     EditText edtCodigo;
     TextView nombres, apellidos, genero, region, especialidad;
@@ -25,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        requestQueue = Volley.newRequestQueue(this);
         edtCodigo = findViewById(R.id.camDoc);
         nombres = findViewById(R.id.txtNombre);
         apellidos = findViewById(R.id.txtApellido);
@@ -33,8 +44,43 @@ public class MainActivity extends AppCompatActivity {
         especialidad = findViewById(R.id.txtEspecialidad);
         btnBuscar = findViewById(R.id.btnConsutar);
 
+        btnBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cargarServicio();
+            }
+        });
+    }
+    private void cargarServicio() {
+        String documento = edtCodigo.getText().toString();
+        String url = "http://192.168.0.42/api/consultarusuario.php?id="+documento;
+        url = url.replace(" ", "%20");
+        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, this, this);
+        requestQueue.add(jsonObjectRequest);
 
+    }
 
+    @Override
+    public void onResponse(JSONObject response) {
+        //respuesta exitosa
+        JSONArray jsonArray = response.optJSONArray("usuario");
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = jsonArray.getJSONObject(0);
+            nombres.setText(jsonObject.optString("nombres"));
+            apellidos.setText(jsonObject.optString("apellidos"));
+            genero.setText(jsonObject.optString("genero"));
+            region.setText(jsonObject.optString("region"));
+            especialidad.setText(jsonObject.optString("especialidad"));
 
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        //error en la respuesta
+        Log.i("Error", error.toString());
     }
 }
