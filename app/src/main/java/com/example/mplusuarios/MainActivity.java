9e19cb7ba9cd292bad169f6e4eaeb51db0c66a97
Alumno <73141051@continental.edu.pe>
2023-05-16 15:03:08 -0500

    commit prueba

--- app/src/main/java/com/example/mplusuarios/MainActivity.java
+++ app/src/main/java/com/example/mplusuarios/MainActivity.java
@@ -3,14 +3,38 @@ package com.example.mplusuarios;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtCodigo;
+    TextView nombres, apellidos, genero, region, especialidad;
+    Button btnBuscar;
+
+    RequestQueue requestQueue; //estudiar realiza una cola de peticiones
+    JsonObjectRequest jsonObjectRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "hola mundo", Toast.LENGTH_SHORT).show();
+
+        edtCodigo = findViewById(R.id.camDoc);
+        nombres = findViewById(R.id.txtNombre);
+        apellidos = findViewById(R.id.txtApellido);
+        genero = findViewById(R.id.txtGenero);
+        region = findViewById(R.id.txtRegion);
+        especialidad = findViewById(R.id.txtEspecialidad);
+        btnBuscar = findViewById(R.id.btnConsutar);
+
+
+
+
     }
 }
